1db6082f7481a27bc6ede984a02420b8cdc1fb24
pkindalov <panayotkindalov@gmail.com>
2018-01-30 17:34:57 +0200

    Added button to clear text from the picture. Added some functionality in watermark.js file. Functionality to clear text, to write text on picture and some little changes like font-size, font

--- scripts/watermarker.js
+++ scripts/watermarker.js
@@ -3,12 +3,76 @@ let browseInputButton = '<input id="fileToUpload" type="file" />';
 
 var CanvasText = new CanvasText;
 
+
+function wrapText(context, text, x, y, maxWidth, lineHeight, color, size) {
+
+ let fontSize = size + "px verdana";
+	
+  let canvas = document.getElementById('processPicture');
+  //serch in receiv string from editor for html tags
+
+
+  //this is varible control defauil context.filltext If variable remain true then default writter write on canvas image
+  var doYouWrite = true;
+
+  text = text.replace('&nbsp', '');
+
+
+  //document.write(bold);
+  var words = text.split(' ');
+  var line = '';
+
+
+
+  for (var n = 0; n < words.length; n++) {
+    var testLine = ' ' + line + words[n] + ' ';
+    var metrics = context.measureText(testLine);
+    var testWidth = metrics.width;
+    if (testWidth > maxWidth && n > 0) {
+
+
+        context.fillText(line, x, y);
+
+      line = words[n] + ' ';
+      y += lineHeight;
+    } else {
+      line = testLine;
+    }
+  }
+
+
+
+
+    if(doYouWrite){
+	  context.font = fontSize;	
+	  context.fillStyle	 = color;
+      context.fillText(line, x, y);
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
 $(document).ready(function(){
 	
 	$('.browseImg').prepend(browseInputButton);
 
 	let canvas = document.getElementById("processPicture");
 	let context = canvas.getContext('2d');
+	let maxWidth = 500;
+    let lineHeight = 20;
+	let exampleText = "Your text here";
+	let x= 490;
+	let y = 450;
 	
 	CanvasText.config({
 		canvas: canvas,
@@ -25,9 +89,14 @@ $(document).ready(function(){
 	
 	imageObj.onload = function(){
 		context.drawImage(imageObj, 0, 0);
+		wrapText(context, exampleText, 40, 40, maxWidth, lineHeight, 'white', 50);
+		
 	}	
 	
 	imageObj.src = examplePicture;
+	
+	
+	
 	
 	
 	$('#fileToUpload').on('change', function(ev) {
@@ -46,4 +115,36 @@ $(document).ready(function(){
 	
 	
 	
+	$('.clearText').click(function(){
+		clearDrawing();
+	});
+	
+	
+	
+	
+	function clearDrawing(){
+     canvas = document.getElementById('processPicture');
+     context = canvas.getContext("2d");
+
+     // clear canvas
+     context.clearRect(0, 0, canvas.width, canvas.height);
+
+     // clear path
+     context.beginPath();
+
+     // use default comp. mode
+     context.globalCompositeOperation = "source-over";
+
+     // reset alpha
+     context.globalAlpha = 1;
+
+     context.drawImage(imageObj, 0, 0, 500, 500);
+     context.globalCompositeOperation = "source-atop";
+   }
+	
+	
+	
 });
+
+
+
